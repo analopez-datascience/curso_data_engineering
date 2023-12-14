cd4d8594ada80d5e851f87743c83d74f5015b902
Ana López <analopezendigital@gmail.com>
2023-12-14 06:45:31 +0000

    corriegiendo el fallo

--- tests/test_fecha.sql
+++
@@ -1,6 +0,0 @@
-SELECT *
-FROM {{ ref('stg_sql_server_dbo__orders') }}
-WHERE delivered_at < estimated_delivery_at
-
-/*checks if there is any case where the delivery date is earlier than the date the order was placed, 
-which would not make logical sense and would indicate some kind of problem with the data +/
