65c68394536a77a143e39f334d9ffbe015ff3733
Ana López <analopezendigital@gmail.com>
2023-12-14 06:37:42 +0000

    actualizando el test

--- tests/test_fecha.sql
+++ tests/test_fecha.sql
@@ -1,4 +1,6 @@
 SELECT *
 FROM {{ ref('stg_sql_server_dbo__orders') }}
-WHERE delivery_date < order_placed_date
-/* comprueba si hay algún caso en el que la fecha de entrega sea anterior a la fecha en que se realizó el pedido, lo que no tendría sentido lógico e indicaría algún tipo de problema con los datos */
+WHERE delivered_at < estimated_delivery_at
+
+/*checks if there is any case where the delivery date is earlier than the date the order was placed, 
+which would not make logical sense and would indicate some kind of problem with the data +/
